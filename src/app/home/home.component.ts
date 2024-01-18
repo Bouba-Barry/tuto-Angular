6f867976049e4744d6fc94f2e1195095eea48646
E7380 <bboubacar366@gmail.com>
2024-01-18 14:34:09 +0100

    first commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,61 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { HousingService } from '../housing.service';
+import { HousingLocation } from '../housinglocation';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, HousingLocationComponent],
+  template: `
+    <section>
+      <form>
+        <input type="text" placeholder="Filter by city" #filter />
+        <button
+          class="primary"
+          type="button"
+          (click)="filterResults(filter.value)"
+        >
+          Search
+        </button>
+      </form>
+    </section>
+    <section class="results">
+      <app-housing-location
+        *ngFor="let housingLocation of filteredLocationList"
+        [housingLocation]="housingLocation"
+      ></app-housing-location>
+    </section>
+  `,
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent {
+  /**
+   * à noter que ci-haut, on va utiliser ngfor pour parcourir et repeter les données qu'on passeras à l'enfant.
+   * ngfor en angular est le meme principe que la boucle for en js
+   */
+  housingLocationList: HousingLocation[] = [];
+  housingService: HousingService = inject(HousingService);
+  filteredLocationList: HousingLocation[] = [];
+
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+    this.filteredLocationList = this.housingLocationList.filter(
+      (housingLocation) =>
+        housingLocation?.city.toLowerCase().includes(text.toLocaleLowerCase())
+    );
+  }
+
+  constructor() {
+    this.housingService
+      .getAllHousingLocations()
+      .then((housingLocationList: HousingLocation[]) => {
+        this.housingLocationList = housingLocationList;
+        this.filteredLocationList = housingLocationList;
+      });
+  }
+}
---
+++ src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,30 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingLocation } from '../housinglocation';
+import { RouterLink, RouterOutlet } from '@angular/router';
+@Component({
+  selector: 'app-housing-location',
+  standalone: true,
+  imports: [CommonModule, RouterLink, RouterOutlet],
+  template: `
+    <section class="listing">
+      <img
+        class="listing-photo"
+        [src]="housingLocation.photo"
+        alt="Exterior photo of {{ housingLocation.name }}"
+      />
+      <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+      <p class="listing-location">
+        {{ housingLocation.city }}, {{ housingLocation.state }}
+      </p>
+      <a [routerLink]="['/details', housingLocation.id]">Learn More</a>
+    </section>
+  `,
+  styleUrls: ['./housing-location.component.css'],
+})
+export class HousingLocationComponent {
+  /** recuperez les params envoyés les props depuis le parent.
+   * Ici on ajoute le ! car l'input s'attend forcement à ce que passe une valeur. on a pas de valeur par defaut.
+   */
+  @Input() housingLocation!: HousingLocation;
+}
